e766190e65945f936aa645ce4cbbaf37bf610c20
Patricio de la Guardia <padlgt01@gmail.com>
2018-03-13 11:08:54 -0400

    Initial commit

---
+++ src/client/components/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      <div>I'm the Best component</div>
+      <button onClick={() => console.log("Hello there!")}>Press me!</button>
+    </div>
+  );
+};
